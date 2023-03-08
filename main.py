0e733094aa61d3d5086aed4d0fcfd0638c087d12
zhuocun <zhuocun.tang@outlook.com>
2023-03-08 15:56:38 +1100

    feat: node 2 situation

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from countPersonTweet import countPersonTweet
 import time
 from collections import Counter
 import heapq
-
+from mpi4py import MPI
 
 with open('./json/twitter-data-small.json', 'r', encoding='utf-8') as f:
     tweet_data_total = json.load(f)
@@ -49,7 +49,34 @@ if node == "1" and core == "8":
 
   for t in threads:
     t.join()
- 
+    
+elif node == "2" and core == "8":
+  total_records = len(tweet_data_total)
+  records_per_thread = total_records // 16
+
+  comm = MPI.COMM_WORLD
+  rank = comm.Get_rank()
+  size = comm.Get_size()
+
+  threads = []
+  if rank == 0:
+    start_time = time.time()
+    for i in range(size-1):
+      start_index = i * records_per_thread
+      length = records_per_thread if i < size-2 else total_records - start_index
+
+      comm.send(tweet_data_total[start_index:start_index + length], dest=i+1)
+  else:
+    chunk = comm.recv(source=0)
+    solve_data(gcc_dict, person_data_dic, chunk, sal_data, author_ids)
+
+  if rank == 0:
+    for i in range(size-1):
+      threads.append(threading.Thread(target=comm.recv, args=(i+1,)))
+      threads[-1].start()
+
+    for t in threads:
+      t.join()
 
 print(f"{'Greater Capital City':<40}{'Numberof Tweets Made'}")
 sorted_gcc_dict = sorted(gcc_dict.items(), key=lambda x: x[1], reverse=True)
